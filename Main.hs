7c839cea79c01ed90d257dbaa300b868a3048b29
Jemil <jemil@DESKTOP-517CRF2.localdomain>
2021-08-04 00:28:26 +0300

    lists functions

--- Main.hs
+++ Main.hs
@@ -52,10 +52,29 @@ d = head [1,2,3,4,5] -- 1
 -- Function composition example 
 headOfTail = head(tail x)
 
-
-
 asc :: Int -> Int -> [Int]
 asc n m
   | m < n = []
   | m == n = [m]
   | m > n = n : asc (n+1) m
+
+-- doubles numbers in the list // double x == [0,2,4,6]
+double nums =
+  if null nums
+    then []
+    else 2 * head nums : double (tail nums)
+
+
+removeOdd numbers =
+  if null numbers
+    then []
+    else 
+      if (mod (head numbers) 2) == 0 --even ?
+      then head numbers : removeOdd (tail numbers)
+      else removeOdd (tail numbers)
+
+-- another syntax
+removeOdd2 numbers
+  | null numbers = []
+  | mod (head numbers) 2 == 0 = head numbers : removeOdd2 (tail numbers)
+  | otherwise = removeOdd2 (tail numbers)
